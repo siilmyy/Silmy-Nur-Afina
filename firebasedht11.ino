821025e3678a04202028ea3dfa54d1bc7958451d
siilmyy <72588375+siilmyy@users.noreply.github.com>
2020-12-18 08:16:27 +0700

    ESP8266 DHT11 Firebase

---
+++ firebasedht11.ino
@@ -0,0 +1,104 @@
+//FirebaseESP8266.h must be included before ESP8266WiFi.h
+#include "FirebaseESP8266.h"  // Install Firebase ESP8266 library
+#include <ESP8266WiFi.h>
+#include <DHT.h>   
+#define FIREBASE_HOST "https://dht11-firebase-efb04-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "hi0yqmpzEYVZcggnZwhqf43XySh6rUInWJYFiJ63"
+#define WIFI_SSID "Tidak ada jaringan"
+#define WIFI_PASSWORD "2631196974iviwulan"
+
+#define DHTPIN D4    // Connect Data pin of DHT to D4
+
+#define DHTTYPE    DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+//Define FirebaseESP8266 data object
+FirebaseData firebaseData;
+
+
+FirebaseJson json;
+
+void setup()
+{
+
+  Serial.begin(9600);
+
+  dht.begin();
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+
+void sensorUpdate(){
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("C  ,"));
+  Serial.print(f);
+  Serial.println(F("F  "));
+
+if (Firebase.setFloat(firebaseData, "Suhu", t))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  if (Firebase.setFloat(firebaseData, "Kelembaban", h))
+  {
+    Serial.println("PASSED");
+    Serial.println("PATH: " + firebaseData.dataPath());
+    Serial.println("TYPE: " + firebaseData.dataType());
+    Serial.println("ETag: " + firebaseData.ETag());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+  else
+  {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + firebaseData.errorReason());
+    Serial.println("------------------------------------");
+    Serial.println();
+  }
+}
+void loop() {
+  sensorUpdate();
+  
+}
